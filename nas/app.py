6d8d49e09aefe358e512c21b3695928ea75e9791
lx <799478793@qq.com>
2024-03-07 15:59:57 +0800

    返回buffer

--- nas/app.py
+++ nas/app.py
@@ -1,26 +1,33 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-from flask import Flask, request
-import urllib.parse, os, requests, m3u8
+import m3u8
+import os
+import re
+import requests
+
 from datetime import datetime
+from io import BytesIO
+
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import unpad
-
+from flask import Flask, request, send_file
 
 app = Flask(__name__)
+
 
 @app.route('/save_video', methods=['POST'])
 def save_video():
     current_date = datetime.now().strftime("%Y%m%d")
-    
+
     data = request.form
-    stream_address = urllib.parse.unquote(data.get('stream_address'))
-    motion_video_time = data.get('motion_video_time')
-    save_path = '/nas'  #视频保存位置
-    video_limit = '30'  #保存天数上限
+    stream_address = data.get('stream_address')
+    motion_video_time = re.sub(r'[^0-9]', '', data.get('motion_video_time'))
+    save_path = '/nas'  # 视频保存位置
+    video_limit = '30'  # 保存天数上限
 
     m3u8_file = f'{save_path}/tmp/{motion_video_time}.m3u8'
     ts_tmp_dir = f'{save_path}/tmp/{motion_video_time}/'
+    save_video_path = f'{save_path}/{current_date}/{motion_video_time}.mp4'
 
     try:
         os.makedirs(f"{save_path}/{current_date}")
@@ -39,22 +46,25 @@ def save_video():
         m3u8_data = f.read()
         m3u8_decode(m3u8_data, ts_tmp_dir)
         try:
-            os.system(f"rm -rf {save_path}/{current_date}/{motion_video_time}.mp4")
-            # 猫眼的视频是HEVC编码格式，有点占用较少，缺点不同的设备不一定能播放出画面
-            # os.system(f"ffmpeg -f concat -safe 0 -i {ts_tmp_dir}ts.list -c copy {save_path}/{current_date}/{motion_video_time}.mp4")
-
+            os.system(f"rm -rf {save_video_path}")
 
             # 软解把HEVC转成H.264，占用较大，软解硬解都能放
-            os.system(f"ffmpeg -f concat -safe 0 -i {ts_tmp_dir}ts.list -c:v libx264 -preset slow -crf 22 -c:a copy -b:a 128k {save_path}/{current_date}/{motion_video_time}.mp4")
+            os.system(f"ffmpeg -f concat -safe 0 -i {ts_tmp_dir}ts.list -c:v libx264 -preset slow -crf 22 -c:a copy -b:a 128k {save_video_path}")
 
-            # 启用VAAPI硬编码
-            # os.system(f"ffmpeg -f concat -safe 0 -i {ts_tmp_dir}ts.list -vaapi_device /dev/dri/renderD128 -c:v h264_vaapi -global_quality 25 -vf 'format=nv12|vaapi,hwupload' -c:a copy {save_path}/{current_date}/{motion_video_time}.mp4")
+            # 读取生成的 MP4 文件为二进制数据
+            with open(f"{save_video_path}", 'rb') as file:
+                mp4_data = BytesIO(file.read())
+
+            # 删除缓存文件
             os.system(f"rm -rf {save_path}/tmp/{motion_video_time}*")
+
+            # 删除超期文件
             os.system(f"find {save_path} -type f -mtime +{video_limit} -delete")
             os.system(f"find {save_path} -depth -type d -empty -delete")
         except:
             pass
-    return "ok"
+    return send_file(mp4_data, mimetype='video/mp4', as_attachment=False)
+
 
 def download_m3u8(url, output_file):
     for i in range(5):
@@ -66,6 +76,7 @@ def download_m3u8(url, output_file):
                 break
         except:
             pass
+
 
 def m3u8_decode(m3u8_data, ts_tmp_dir):
     m3u8_obj = m3u8.loads(m3u8_data)
@@ -87,6 +98,7 @@ def m3u8_decode(m3u8_data, ts_tmp_dir):
             f.write(f"file '{ts_tmp_dir}{i}.ts'\n")
     return True
 
+
 def download_key(key_url):
     for i in range(5):
         try:
@@ -94,6 +106,7 @@ def download_key(key_url):
             return response.content
         except:
             pass
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5005)
